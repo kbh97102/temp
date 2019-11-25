069cf37fa69d9e9994c336b546422e3cd6030ed8
Arakene <kbh97102@naver.com>
2019-11-25 19:39:03 +0900

    저장을잘합시다

--- ThreadTest/src/th/Main.java
+++ ThreadTest/src/th/Main.java
@@ -38,6 +38,7 @@ class MainFrame {
     private JPanel p1;
     private ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
     private JButton btn;
+
     public MainFrame() {
         frame = new JFrame();
         panel = new JPanel();
@@ -46,7 +47,7 @@ class MainFrame {
         panel.setLayout(null);
         p1 = new JPanel();
 
-        btn =new JButton("Click");
+        btn = new JButton("Click");
 
         p1.setLayout(new BorderLayout());
         p1.add(panel, BorderLayout.CENTER);
@@ -54,66 +55,71 @@ class MainFrame {
         p1.add(btn, BorderLayout.EAST);
 
 
-
-
-
-
-        Runnable t1 = () -> {
-            synchronized (this){
+        Runnable makeWordTask = () -> {
+            synchronized (this) {
                 randomIndex = (int) (Math.random() * 1000);
-                enemy.add(new Enemy(randomIndex, 0, count + ""));
-                panel.add(enemy.get(count++).getImg());
-                System.out.println("iswork");
-
-                panel.revalidate();
-                panel.repaint();
-            }
-        };
-        Runnable t2 = () -> {
-            for (int i = 0; i < enemy.size(); i++) {
-                Enemy e = enemy.get(i);
-                if (e.getY() > 600) {
-                    enemy.remove(e);
+                int blindGenerate = (int)(Math.random()*100);
+                if(blindGenerate >= 90){
+                    Enemy blindEnemy = new Enemy(randomIndex, 0, path,true);
+                    blindEnemy.setBlindTTL(5);
+                    enemy.add(blindEnemy);
                 }
-                e.down();
+                else{
+                    enemy.add(new Enemy(randomIndex, 0, count + "", true));
+                }
+                panel.add(enemy.get(count++).getImg());
+
             }
         };
-        Runnable t3 = () ->{
-            tf.addActionListener(event -> {
-                String input = tf.getText();
-                for (int i = 0; i < enemy.size(); i++) {
-                    if (enemy.get(i).word().equals(input)) {
-                        panel.remove(enemy.get(i).getImg());
-                        //여기가문제
-                        enemy.remove(i);
-                        panel.revalidate();
-                        panel.repaint();
-                        break;
+        Runnable moveTask = () -> {
+            for (int i = 0; i < enemy.size(); i++) {
+                if(enemy.get(i).getIsAlive()){
+                    if(enemy.get(i).getBlindTTL()!=0){
+                        enemy.get(i).decreaseBlindTTL();
+                    }
+                    if(enemy.get(i).getY() > 400){
+                        enemy.get(i).changeIsAlive(false);
+//                        panel.remove(enemy.get(i).getImg());
+//                        panel.revalidate();
+//                        panel.repaint();
+                    }else{
+                        enemy.get(i).down();
                     }
                 }
-                tf.setText("");
-            });
+
+            }
         };
-        Thread test1 = new Thread(t1);
-        Thread test2 = new Thread(t2);
-        Thread test3 = new Thread(t3);
 
-        btn.addActionListener(event -> {
-            if(test1.isInterrupted()){
-                System.out.println("test1 is down");
+        tf.addActionListener(event -> {
+            String input = tf.getText();
+            for (int i = 0; i < enemy.size(); i++) {
+                if (enemy.get(i).word().equals(input)) {
+                    enemy.get(i).changeIsAlive(false);
+//                    panel.remove(enemy.get(i).getImg());
+//                    panel.revalidate();
+//                    panel.repaint();
+                }
             }
-            else{
-                System.out.println("is alive");
-                System.out.println(test1.getState());
-            }
-
+            tf.setText("");
         });
 
+        Runnable display = () ->{
+            for(int i=0;i<enemy.size();i++){
+                if(enemy.get(i).getIsAlive()){
+                    enemy.get(i).draw();
+                }
+                else{
+                    panel.remove(enemy.get(i).getImg());
+                    panel.revalidate();
+                    panel.repaint();
+                }
+            }
+        };
 
 
-        service.scheduleWithFixedDelay(test1, 0, 1, TimeUnit.SECONDS);
-        service.scheduleWithFixedDelay(test2, 0, 500, TimeUnit.MILLISECONDS);
-        service.submit(test3);
+        service.scheduleWithFixedDelay(makeWordTask, 0, 1, TimeUnit.SECONDS);
+        service.scheduleWithFixedDelay(moveTask, 0, 500, TimeUnit.MILLISECONDS);
+        service.scheduleWithFixedDelay(display, 0, 14, TimeUnit.MILLISECONDS);
 
 
         frame.add(p1);
@@ -131,12 +137,16 @@ class Enemy {
     private String path;
     private JLabel img;
     private int downSpeed = 20;
+    private int blindTTL = 0;
+    private int blindDecrease = 1;
+    private boolean isAlive;
 
-    public Enemy(int x, int y, String path) {
+    public Enemy(int x, int y, String path, boolean isAlive) {
         img = new JLabel();
         this.x = x;
         this.y = y;
         this.path = path;
+        this.isAlive = isAlive;
         img.setText(path);
         img.setBounds(x, y, 100, 20);
 //        img.setIcon(new ImageIcon(path));
@@ -153,11 +163,27 @@ class Enemy {
 
     public void down() {
         y += downSpeed;
-        img.setLocation(x, y);
+    }
+    public void draw(){
+        img.setLocation(x,y);
     }
 
     public int getY() {
         return y;
     }
-
+    public boolean getIsAlive(){
+        return isAlive;
+    }
+    public void changeIsAlive(boolean isAlive){
+        this.isAlive = isAlive;
+    }
+    public void setBlindTTL(int TTL){
+        this.blindTTL = TTL;
+    }
+    public void decreaseBlindTTL(){
+        blindTTL -= blindDecrease;
+    }
+    public int getBlindTTL(){
+        return blindTTL;
+    }
 }
